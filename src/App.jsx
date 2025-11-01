3715d81c369030ec90f1479b1308a409ba5c78ef
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-11-02 01:50:04 +0530

    GSAP animations in HeroSection; register ScrollTrigger in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all'
+import gsap from 'gsap';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar/>
       <HeroSection/>
+      <div className='h-dvh border border-e-dark-brown '></div>
     </main>
   )
 }
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -27,7 +27,28 @@ const HeroSection = () => {
         },
         "-=0.5"
       )
-      .from(titleSplit.chars());
+      .from(titleSplit.chars,{
+        yPercent:200,
+        stagger:0.02,
+        ease:'power2.out'
+      },"-=0.5")
+
+
+      const heroTl = gsap.timeline({
+        scrollTrigger:{
+            trigger:'.hero-container',
+            start:'1% top',
+            end:'bottom top',
+            scrub: true,
+        }
+      })
+
+      heroTl.to(".hero-container",{
+        rotate:7,
+        scale:0.9,
+        yPercent:30,
+        ease:'power1.inOut'
+      })
   });
 
   return (
