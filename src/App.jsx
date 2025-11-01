abf0e3ea2855f824ca4f1f3bd7e5c941c4902a85
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-11-02 01:21:56 +0530

    Implement Navbar and HeroSection components; update App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <Navbar/>
+      <HeroSection/>
+    </main>
   )
 }
 
