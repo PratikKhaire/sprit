622ba48dce3c1c49680a3d84f660996cad82c2d6
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-11-03 16:50:41 +0530

    Add FlavorSection, FlavorTitle, and FlavorSlider components with GSAP animations

--- src/sections/FlavorSection.jsx
+++ src/sections/FlavorSection.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import FlavorTitle from '../components/FlavorTitle'
+import FlavorSlider from '../components/FlavorSlider'
 
 const FlavorSection = () => {
-  return <section className=' flavor-section'>
+  return <section className='flavor-section'>
     <div className=' h-full flex lg:flex-row flex-col items-center relative'>
-        <div className='lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:-mt-0'></div>
+        <div className='lg:w-[57%] flex-none h-80 lg:h-full md:mt-20 xl:mt-0'>
+          <FlavorTitle/>
+        </div>
+        <div className="h-full">
+          <FlavorSlider/>
+        </div>
     </div>
   </section>
 }
