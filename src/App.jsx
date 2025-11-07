3887f482dc02f1f0d53f67eb9d3058ca0f288624
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-11-07 17:01:43 +0530

    Add TestimonialSection component with GSAP animations for enhanced user engagement

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import FlavorSection from './sections/FlavorSection';
 import { useGSAP } from '@gsap/react';
 import NutritionSection from './sections/NutritionSection';
 import BenefitSection from './sections/BenefitSection';
+import TestimonialSection from './sections/TestimonialSection';
 
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
 
@@ -30,6 +31,7 @@ const App = () => {
        <NutritionSection/>
       <div>
             <BenefitSection />
+              <TestimonialSection />
 
           </div>
       </div> </div>
